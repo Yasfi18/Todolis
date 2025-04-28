238821317be372124db73b19fe20af0f92e5db34
Yasfi <yasfiganteng581@gmail.com>
2025-04-28 08:52:37 +0700

    update

--- main.js
+++ main.js
@@ -1,36 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  getDoc,
-  deleteDoc,
-  doc,
-  query,
-  orderBy,
-  updateDoc
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBbJDnmlNa2wObKWmQ6MLuXQ5KWbp0FDEA",
-  authDomain: "insan-cemerlang-8011b.firebaseapp.com",
-  projectId: "insan-cemerlang-8011b",
-  storageBucket: "insan-cemerlang-8011b.firebasestorage.app",
-  messagingSenderId: "642542638808",
-  appId: "1:642542638808:web:8dae3c0d85e3be8c5c29ad"
-};
-
-
-// Inisialisasi Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export async function ambiltoDOList() {
-  const refDokumen = collection(db, "toDOList");
-  const kueri = query(refDokumen, orderBy("toDOList"));
-  const cuplikanKueri = await getDocs(kueri);
-  
   let hasil = [];
   cuplikanKueri.forEach((dok) => {
     hasil.push({
